efd1969e54d29cec8eabff3f0072069da76cc8a5
MAZTRO <design.mallet@gmail.com>
2020-05-28 18:51:30 -0500

    Add some buttoms

---
+++ Web/scripts/configBlock.js
@@ -0,0 +1,20 @@
+import Calendar from 'react-calendar';
+
+class MyApp extends Component {
+  state = {
+    date: new Date(),
+  }
+
+  onChange = date => this.setState({ date })
+
+  render() {
+    return (
+      <div>
+        <Calendar
+          onChange={this.onChange}
+          value={this.state.date}
+        />
+      </div>
+    );
+  }
+}
