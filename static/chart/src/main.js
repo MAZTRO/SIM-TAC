c53112e659a1609ba4a14ec9c51b2641466c0710
cristian gomez <1154@holbertonschool.com>
2020-06-09 23:17:38 +0000

    all routes are functional

--- static/chart/src/main.js
+++ static/chart/src/main.js
@@ -5,11 +5,11 @@ window.tvWidget = new TradingView.widget({
     symbol: 'Bitfinex:BTC/USD', // default symbol
     interval: '2', // default interval
     timezone: "America/New_York",
-    disabled_features: ["header_saveload", "header_compare", "header_screenshot", "header_fullscreen_button", /* "header_symbol_search" */],
+    disabled_features: ["header_saveload", "header_compare", "header_screenshot", "header_fullscreen_button"],
     enabled_features: ["hide_left_toolbar_by_default", "countdown"],
-    fullscreen: false, // displays the chart in the fullscreen mode
+    fullscreen: false,
     container_id: 'tvChart',
     datafeed: Datafeed,
     autosize: true,
-    library_path: 'chart/charting_library',
+    library_path: 'static/chart/charting_library/',
 });
