0d05ee2d7c3f6e0470b6bac0245455c6ef75d027
cristian gomez <1154@holbertonschool.com>
2020-06-10 21:42:45 +0000

    added login logout and signup functionallity. login and signup css corrections needed

--- SIMTAC/settings.py
+++ SIMTAC/settings.py
@@ -135,3 +135,7 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
+
+LOGIN_URL = '/users/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = LOGIN_URL
--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 """Users model"""
 from django.db import models
+from django.contrib.auth.models import User
 
+class Profile(models.Model):
+    """model profile"""
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    biography = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """String representation of the object"""
+        return self.user.username
